60b148c472e012b4a64ab5a0def7a53f71f96188
Evan Leahy <49353272+echolimauw@users.noreply.github.com>
2020-08-30 22:43:11 -0700

    While loops, floats, doubles, if/else checks

---
+++ IncrementOperatorsI.cpp
@@ -0,0 +1,49 @@
+// IncrementOperatorsI.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double NumberPi = 3.14159;
+	double NumberE = 2.718281828;
+
+	int count = 0;
+
+	bool Condition = true;
+	
+	do
+	{
+		cout << "The number Pi is: " << NumberPi << endl;
+		cout << "The number E is: " << NumberE << endl;
+		cout << "Count: " << NumberPi + NumberE * count;
+
+		cout << "Pi + E*Count = " << NumberPi + NumberE * count << endl;
+
+		count++;
+		if (count <= 100)
+		{
+			Condition = true;
+		}
+		else
+		{
+			Condition = false;
+		}		
+	} while (Condition);
+
+
+	system("pause");
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
